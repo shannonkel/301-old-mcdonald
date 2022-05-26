163130572fd4ffb7ba253da4f5a69480206904da
skelavos <shannon_kelavos@intuit.com>
2022-05-26 07:42:25 -0700

    interactive

---
+++ app.py
@@ -0,0 +1,64 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+########### Define your variables ######
+
+# here's the list of possible columns to choose from.
+list_of_columns =['year', 'city', 'state', 'race', 'killed', 'injured', 'victims', 'date', 'type', 'time']
+
+mycolumn='year'
+#myheading1 = f"Wow! That's a lot of {mycolumn}!"
+mygraphtitle = 'School Shootings over a 10 year period'
+mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+mycolorbartitle = "Hundreds"
+tabtitle = 'Something has to change'
+sourceurl = 'https://plot.ly/python/choropleth-maps/'
+githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
+
+
+########## Set up the chart
+
+import pandas as pd
+df = pd.read_csv('assets/CNN_School_Shooting_File_7_26_2019.csv')
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df[mycolumn].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = mycolorscale,
+    colorbar_title = mycolorbartitle,
+))
+
+fig.update_layout(
+    title_text = mygraphtitle,
+    geo_scope='usa',
+    width=1200,
+    height=800
+)
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+
+app.layout = html.Div(children=[
+    html.H1(myheading1),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
+)
+
+############ Deploy
+if __name__ == '__main__':
+    app.run_server()
