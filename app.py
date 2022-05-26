b2a2454a40d567d8ce0524bf0d6e231812143a47
skelavos <shannon_kelavos@intuit.com>
2022-05-26 15:39:41 -0700

    shannon update

--- app.py
+++ app.py
@@ -1,45 +1,57 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
+import plotly.graph_objects as go
 import pandas as pd
 
 ########### Define your variables ######
 
 # here's the list of possible columns to choose from.
-list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 'poultry',
-       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+list_of_columns =['city', 'state', 'killed', 'injured', 'date', 'type', 'time']
 
-mycolumn='corn'
-myheading1 = f"Wow! That's a lot of {mycolumn}!"
-mygraphtitle = '2011 US Agriculture Exports by State'
-mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-mycolorbartitle = "Millions USD"
-tabtitle = 'Old McDonald'
-sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
+# mycolumn='state'
+# #myheading1 = f"Wow! That's a lot of {mycolumn}!"
+# mygraphtitle = 'School Shootings over a 10 year period'
+# mycolorscale = 'Reds' # Note: The error message will list possible color scales.
+# mycolorbartitle = "Hundreds"
+# tabtitle = 'Something has to change'
+# sourceurl = 'https://plot.ly/python/choropleth-maps/'
+# githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 
 
 ########## Set up the chart
 
-import pandas as pd
-df = pd.read_csv('assets/usa-2011-agriculture.csv')
+
+df = pd.read_csv('./assets/CNN_School_Shooting_File_7_26_2019.csv')
+
+for col in df.columns:
+    df[col] = df[col].astype(str)
+
+df['text'] = df['date'] + 'time' + df['time'] + '<br>' + \
+    'City' + df['city'] + 'State' + df['state'] + '<br>' + \
+    'Deaths' + df['killed'] + 'Injuries' + df['injured'] + '<br>' + \
+    'School Type' + df['type']
 
 fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
+    locations=df['code'],
+    z=df['total exports'].astype(float),
+    locationmode='USA-states',
+    colorscale='Reds',
+    autocolorscale=False,
+    text=df['text'], # hover text
+    marker_line_color='white', # line markers between states
+    colorbar_title="Millions USD"
 ))
 
 fig.update_layout(
-    title_text = mygraphtitle,
-    geo_scope='usa',
-    width=1200,
-    height=800
+    title_text='2011 US Agriculture Exports by State<br>(Hover for breakdown)',
+    geo = dict(
+        scope='usa',
+        projection=go.layout.geo.Projection(type = 'albers usa'),
+        showlakes=True, # lakes
+        lakecolor='rgb(255, 255, 255)'),
 )
+
+fig.show()
+
+
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
