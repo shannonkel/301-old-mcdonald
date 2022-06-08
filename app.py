e718beed1f6fcc19bdc9464665d701d5afed9f83
skelavos <shannon_kelavos@intuit.com>
2022-06-08 15:39:53 -0700

    first update

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 
        'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
-mycolumn='corn'
+mycolumn='beef'
 myheading1 = f"Wow! That's a lot of {mycolumn}!"
 mygraphtitle = '2011 US Agriculture Exports by State'
-mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+mycolorscale = 'turbo' # Note: The error message will list possible color scales.
 mycolorbartitle = "Millions USD"
-tabtitle = 'Old McDonald'
+tabtitle = 'US Agriculture'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
+githublink = 'https://github.com/shannonkel/301-old-mcdonald'
 
 
 ########## Set up the chart
