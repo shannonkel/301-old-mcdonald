9df6ed9e42f24c1eae7f9751ee6f824f77b8c4c6
skelavos <shannon_kelavos@intuit.com>
2022-06-08 15:04:21 -0700

    starting over

--- app.py
+++ app.py
@@ -1,39 +1,66 @@
-import plotly.graph_objects as go
-import numpy as np
-  
-  
-# creating random data through randomint
-# function of numpy.random
-np.random.seed(42)
-  
-random_x = np.random.randint(1, 101, 100)
-random_y = np.random.randint(1, 101, 100)
-  
-plot = go.Figure(data=[go.Scatter(
-    x=random_x,
-    y=random_y,
-    mode='markers',)
-])
-  
-# Add dropdown
-plot.update_layout(
-    updatemenus=[
-        dict(
-            buttons=list([
-                dict(
-                    args=["type", "scatter"],
-                    label="Scatter Plot",
-                    method="restyle"
-                ),
-                dict(
-                    args=["type", "bar"],
-                    label="Bar Chart",
-                    method="restyle"
-                )
-            ]),
-            direction="down",
-        ),
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+########### Define your variables ######
+
+# here's the list of possible columns to choose from.
+list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 'poultry',
+       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
+       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+
+mycolumn='corn'
+myheading1 = f"Wow! That's a lot of {mycolumn}!"
+mygraphtitle = '2011 US Agriculture Exports by State'
+mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+mycolorbartitle = "Millions USD"
+tabtitle = 'Old McDonald'
+sourceurl = 'https://plot.ly/python/choropleth-maps/'
+githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
+
+
+########## Set up the chart
+
+import pandas as pd
+df = pd.read_csv('assets/usa-2011-agriculture.csv')
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df[mycolumn].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = mycolorscale,
+    colorbar_title = mycolorbartitle,
+))
+
+fig.update_layout(
+    title_text = mygraphtitle,
+    geo_scope='usa',
+    width=1200,
+    height=800
+)
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+
+app.layout = html.Div(children=[
+    html.H1(myheading1),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
     ]
 )
-  
-plot.show()
+
+############ Deploy
+if __name__ == '__main__':
+    app.run_server()
